841f7b0135b8ff2efe692f7f4ad434e0f190ca60
agapinank <agapinank@gmail.com>
2021-10-21 15:38:28 -0700

    passport authentication

--- app.js
+++ app.js
@@ -1,82 +1,105 @@
 //jshint esversion:6
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encryp = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const port = 3000;
 
-
-const app= express();
+const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 //object created from mongoose schema class
-const userSchema= new mongoose.Schema({
-    email: String,
-    password: String,
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
 });
-
-userSchema.plugin(encryp,{secret:process.env.SECRET, encryptedFields: ['password']});
+//salts and hash users to database
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/",(req,res)=>{
-    res.render("home");
+// create local login strategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
-app.get("/login",(req,res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
-app.get("/register",(req,res)=>{
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 //user creation and saves to userDB
-app.post("/register",(req,res)=>{
-  
-    const newUser = new User({
-        email: req.body.username,
-        password:req.body.password
-    });
-    newUser.save((err)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets")
-        }
-    });
-});
-app.post("/login",(req,res)=>{
-    //check DB user credentials
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    //search DB user by user {email:username}
-
-    User.findOne({email:username}, (err,foundUser)=>{
-        if(err){
-            console.log(err);
-        }else{
-            // if found & match to user password = access 
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
-            }
-        }
-    });
-
-
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
 });
 
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-
-
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
